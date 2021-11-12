b804f7fcaaf39905649642cf782bfa2c37dc68b9
moizamanzoor <90446178+moizamanzoor@users.noreply.github.com>
2021-11-12 16:17:46 -0500

    division code and tests for division and w/ 0

--- tests/divsion_zero_test.py
+++ tests/divsion_zero_test.py
@@ -5,8 +5,8 @@ from calc.calculations.division import Division
 def test_calculation_division():
     """testing that our calculator returns 'error' when dividing by zer0"""
     #Arrange
-    mynumbers = (4.0,0.0)
-    division = Division(mynumbers)
+    my_numbers = (4.0,0.0)
+    division = Division(my_numbers)
     #Act
     #Assert
     assert division.get_result() == ZeroDivisionError
--- tests/subtraction_test.py
+++ tests/subtraction_test.py
@@ -4,10 +4,11 @@ from calc.calculations.subtraction import Subtraction
 def test_calculation_subtraction():
     """testing that our calculator has a static method for addition"""
     #Arrange
-    mynumbers = (2.0,1.0)
-    subtraction = Subtraction(mynumbers)
+    my_numbers = (2.0,1.0)
+    subtraction = Subtraction(my_numbers)
     #Act
     #Assert
     assert subtraction.get_result() == 1
+
 
 
